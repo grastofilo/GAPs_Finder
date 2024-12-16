4567c4ab397352065760ecf69aafbad2e65018e6
grastofilo <mail@lucaloiacono.com>
2024-12-16 20:16:50 +0100

    Delete pages/disclaimer.py

--- pages/disclaimer.py
+++
@@ -1,18 +0,0 @@
-import streamlit as st
-
-st.title("Disclaimer:")
-
-st.markdown('L'applicazione è progettata per fornire informazioni sullo storico delle giornate in  gap delle azioni, elaborando i  dati disponibili gratuitamente in rete.
-I risultati forniti hanno una finalità puramente informativa e non costituiscono in alcun modo consulenza finanziaria, invito o istigazione a intraprendere attività di trading.
-
-L'applicazione non garantisce l'accuratezza e la completezza delle informazioni fornite. Le informazioni potrebbero inoltre non essere adeguate per tutti gli utenti; ogni decisione dovrebbe essere valutata in autonomia.
-L'utente è interamente responsabile delle proprie decisioni e delle eventuali conseguenze derivanti dall’ utilizzo delle informazioni fornite dall'app. 
-L'autore dell'applicazione non si assume di conseguenza alcuna responsabilità per eventuali perdite o danni derivanti dalle decisioni prese dagli utenti sulla base delle analisi fornite dall’app.')
-
-st.markdown('This application is designed to provide information on the historical gap days of stocks, processing freely available data online. 
-The results provided are for informational purposes only and do not constitute financial advice,
-an invitation, or an encouragement to engage in trading activities.
-The application does not guarantee the accuracy or completeness of the information provided. 
-Additionally, the information may not be suitable for all users; each decision should be independently evaluated. 
-Users are fully responsible for their decisions and any consequences arising from the use of the information provided by the app.
-The author of the application therefore assumes no responsibility for any losses or damages resulting from users' decisions based on the analyses provided by the app.')
